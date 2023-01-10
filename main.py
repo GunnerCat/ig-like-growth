425126984269b9c03fa732b132ffa2365620b85f
Pieter Riyanto <ptrryt@gmail.com>
2023-01-10 17:50:54 +0700

    add time for each tick in excel

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 URL  = ""
-page = None
-soup = None
 likeCnt = []
 def init():
     # start by defining the options 
@@ -21,27 +19,37 @@ def init():
     # this returns the path web driver downloaded 
     chrome_path = ChromeDriverManager().install() 
     chrome_service = Service(chrome_path) 
-    # pass the defined options and service objects to initialize the web driver 
+    
     driver = Chrome(options=options, service=chrome_service) 
     driver.implicitly_wait(5)
-    print("please enter the url")
+    print("Please input your directed post")
+    URL = input()   
+    print("Please input interval per scanning (in second): ")
+    interval = input()
     
-    URL = input()
 
+    driver.get(URL) 
+    fileName = driver.find_element(By.CSS_SELECTOR,"a[class='x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz _acan _aiit _acao _aija _acat _acaw _aj1- _a6hd']").text + str((time.time()))+".xlsx"
+    f = open(fileName,"a")
+    f.close()
+    
+    
     while(True):
         try:
-            driver.get(URL) 
             content = driver.find_element(By.CSS_SELECTOR,"div[class='_aacl _aaco _aacw _aacx _aada _aade']>span")
             print(content.text)
-            
-            likeCnt.append(content.text)
+            curTime = str(time.localtime().tm_hour)+str(time.localtime().tm_min)+str(time.localtime().tm_sec)
+            tempLikeStorage=[int(content.text),str(curTime)]
+            likeCnt.append(tempLikeStorage)
             df = pd.DataFrame(likeCnt)
-            writer = pd.ExcelWriter('test.xlsx', engine='xlsxwriter')
+            writer = pd.ExcelWriter(fileName, engine='xlsxwriter')
             df.to_excel(writer, sheet_name='welcome', index=False)
             writer.save()
             
-            time.sleep(3)
+            time.sleep(interval)
+            driver.get(URL) 
         except:
+            print("refresh")
             driver.refresh()
             
     
